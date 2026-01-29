3b3d53f493882561537a84620c6a74d24517c2ea
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2026-01-30 00:35:29 +0530

    29/01/2026
    Introduction and  install NumPy, Difference  between NumPy and list.

---
+++ NumPy/day1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+#num_array_ print without comma  seprated
+x=np.array([1,2,3,4,5])
+print(x)
+print(type(x))  #numpy.ndarray
+
+
+
+#list_array _ print with comma seprated
+y=[6,7,8,9,10]
+print(type(y))  #list
+print(y)
+
+
