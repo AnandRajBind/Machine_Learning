c8f463d364ac55de2239d32a7a1378b18f0c3819
Anand Raj Bind <116335833+AnandRajBind@users.noreply.github.com>
2026-01-29 22:58:56 +0530

    29/01/2026
    input, output , type Casting

---
+++ Python/day1.py
@@ -0,0 +1,11 @@
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+marks=float(input("Enter your marks:"))
+
+print("Name", name)
+print("Age", age)
+print("Marks", marks)
+
+sum=age+marks
+print("Sum of age and marks:", sum)
+print("Type of sum is", type(sum))
